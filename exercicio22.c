297d77f92dda63ac57b06ac931f6a371387e45ca
Marcos Glasner <marcosglasner@gmail.com>
2023-11-30 17:58:25 +0000

    The program still under implementation.

--- exercicio22.c
+++ exercicio22.c
@@ -9,8 +9,8 @@
 int main(){
 
     FILE *arq1; FILE *arq2; FILE *aux1; FILE *aux2;
-    int nota, notas[3][3], cont = 0;
-    char alunos[3][20], entrada[20], saida[20], ch;
+    int nota[4], notas[4][3], i = 0;
+    char alunos[4][20], entrada[20], saida[20], ch, linha[1000][1000];
 
     printf("Qual o nome do arquivo de entrada? ");
     scanf("%s", &entrada);
@@ -23,7 +23,6 @@ int main(){
     aux2 = fopen("texto22_aux2.txt", "w");
 
     if(arq1){
-        int *ptr = &ptr;
         while((ch = fgetc(arq1)) != EOF) {
             if(ch == 'A' || ch == 'B' || ch == 'C' || ch == 'D' || ch == 'E' || ch == 'F'
             || ch == 'G' || ch == 'H' || ch == 'I' || ch == 'J' || ch == 'L' || ch == 'M'
@@ -32,8 +31,9 @@ int main(){
             || ch == 'Z'){
                 fputc(ch, aux1);
             }if(ch == '0' || ch == '1' || ch == '2' || ch == '3' || ch == '4' || ch == '5'
-            || ch == '6' || ch == '7' || ch == '8' || ch == '9'){
+            || ch == '6' || ch == '7' || ch == '8' || ch == '9') {
                 fputc(ch, aux2);
+
             }if(ch == '\n'){
                 fputc(ch, aux1);
                 fputc(ch, aux2);
@@ -42,6 +42,22 @@ int main(){
 
     }else{
         printf("Arquivo não encontrado!");
+    }
+
+    fclose(aux1);
+    fclose(aux2);
+
+    aux1 = fopen("texto22_aux1.txt", "r");
+    aux2 = fopen("texto22_aux2.txt", "r");
+
+    while((fgets(linha[100], sizeof(linha[100]), aux2)) != NULL) {
+        if(i >= 4){
+            break;
+        }
+        nota[i] = atoi(linha[100]);
+        printf("%d\n", nota[i]);
+        i++;
+
     }
 
     fclose(arq1);
